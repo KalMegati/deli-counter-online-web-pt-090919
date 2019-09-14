01b4b2bef02ad85b3c5524b9bea2fba5829ae0f9
Srikalyan Megati <kalmegati@gmail.com>
2019-09-14 02:37:32 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,5 +15,5 @@ def line(array)
 end
 
 def take_a_number(array, name)
-  puts "Welcome, #{name}. You are number #{} in line."
+  puts "Welcome, #{name}. You are number #{array.length} in line."
 end
